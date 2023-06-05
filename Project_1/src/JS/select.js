9ef68be82ee1a1ec980b2f48e4eb7fa23d634cf6
su12 <95666574+su0797@users.noreply.github.com>
2023-06-05 23:16:29 +0900

    Add files via upload

--- Project_1/src/JS/select.js
+++ Project_1/src/JS/select.js
@@ -1,30 +1,17 @@
-const $input = document.querySelector("input");
+const $button = document.querySelector("#select_btn");
+const $search = document.querySelector("searchInput");
 
 // 사용자가 선택한 조건을 입력하는 함수
-$input.addEventListener("button", (e) => {
-    selectvalue = e.target.value; 
+$button.addEventListener("click", (e) => {
+    selectli = e.target.value; 
+    console.log(selectli);
 });
 
 const selectValue = (selectli) => {
     let selectButton = document.createElement("li");
     // selectButton.classList.add("selectli");
     selectButton.innerText = selectli;
-    $input.appendChild(selectButton);
+    $search.appendChild(selectli);
     console.log(selectli);
 };
 
-// document.querySelectorAll('button[type="button"]').forEach(selectButton=> {
-//     selectButton.addEventListener("change", (e) => {
-//         const target = e.target;
-//         const id = target.id;
-//         const checked = target.checked;
-
-//         if (id === '여자' || id === '남자') {
-//         gender = checked ? id : '';
-//         } else if (id.includes('대')) {
-//         age = checked ? id : '';
-//         } else if (id === '실내' || id === '실외') {
-//         place = checked ? id : '';
-//         }
-//     });
-// });
