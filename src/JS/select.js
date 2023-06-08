89530b06fc284a069df931e51baed0dd50c357b0
su12 <ts04109@naver.com>
2023-06-09 02:07:41 +0900

    폴더 전체 정리 후 수정

---
+++ src/JS/select.js
@@ -0,0 +1,20 @@
+// const $button = document.getElementById("select_btn");
+
+// const $search = document.querySelector("searchInput");
+// document.querySelector('li').setAttribute("id", "select_btn_two");
+
+// const selectValue = () => {
+//     document.querySelector('li').setAttribute("id", "select_btn_two");
+//     // selectButton.classList.add("selectli");
+//     // selectButton.innerText = ID;
+//     // // $button.append(ID);
+//     // console.log("ID");
+// };
+
+// 사용자가 선택한 조건을 입력하는 함수
+// $button.addEventListener("click", (e) => {
+//     selectli = e.target.value; 
+//     console.log(selectli);
+// });
+
+
