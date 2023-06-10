5335f3130dbe72870c32a44cd18d82e4bd300cbf
su12 <ts04109@naver.com>
2023-06-10 23:39:30 +0900

    버튼 기능 select.js로 분리

--- src/JS/search.js
+++ src/JS/search.js
@@ -12,27 +12,7 @@ let genre = '';
 let weather = '';
 let time = '';
 
-// 선택한 값을 질문으로 넣기
-document.querySelectorAll('input[type="radio"]').forEach((radio) => {
-    radio.addEventListener("change", (e) => {
-            const target = e.target;
-            const id = target.id;
-            const value = target.value;
 
-            if (id === "1980년대" || id === "1990년대" || id === "2000년대" || id === "2010년대" || id === "2020년대" || id === "기타") {
-                year = value ? id : '';
-            } else if (id === "발라드" || id === "댄스" || id === "랩,힙합" || id === "R&B,Soul" || id === "인디음악" || id === "록,메탈" || id === "트로트" || id === "포크,블루스" || id === "기타" ) {
-                genre = value ? id : '';
-            } else if (id === "화창한날" || id === "비가내리는날" || id === "안개가가득한날" || id === "눈이내리는날" || id === "바람이부는날" || id === "기타" ) {
-                weather = value ? id : '';
-            } else if (id === "아침" || id === "점심" || id === "저녁" || id === "새벽" || id === "기타" ) {
-                time = value ? id : '';
-            }
-            const selectValue = [year, genre, weather, time];
-            question = selectValue.join();
-            console.log(question)
-    })
-})
 
 // 질문과 답변 저장
 let data = [
@@ -65,28 +45,6 @@ let data = [
 
 // 화면에 뿌려줄 데이터, 질문들
 let questionData = [];
-
-// input에 입력된 질문 받아오는 함수
-// $input.addEventListener("check", (e) => {
-//     question = e.target.value;
-//     console.log(question);
-//     });
-
-// document.querySelectorAll('input[type="radio"]').forEach((checkbox) => {
-//     checkbox.addEventListener("change", (e) => {
-//         const target = e.target;
-//         const id = target.id;
-//         const checked = target.value;
-//         question = id;
-//         console.log(question)
-
-//     });
-// });
-
- // input에 입력된 질문 받아오는 함수
-// $yearSection.addEventListener("change", (e) => {
-//     question = e.target.value;
-//     });
 
 // 사용자의 질문을 객체를 만들어서 push
 const sendQuestion = (question) => {
--- src/JS/select.js
+++ src/JS/select.js
@@ -1,20 +1,22 @@
-// const $button = document.getElementById("select_btn");
+// 선택한 값을 질문으로 넣기
+document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+    radio.addEventListener("change", (e) => {
+            const target = e.target;
+            const id = target.id;
+            const value = target.value;
 
-// const $search = document.querySelector("searchInput");
-// document.querySelector('li').setAttribute("id", "select_btn_two");
-
-// const selectValue = () => {
-//     document.querySelector('li').setAttribute("id", "select_btn_two");
-//     // selectButton.classList.add("selectli");
-//     // selectButton.innerText = ID;
-//     // // $button.append(ID);
-//     // console.log("ID");
-// };
-
-// 사용자가 선택한 조건을 입력하는 함수
-// $button.addEventListener("click", (e) => {
-//     selectli = e.target.value; 
-//     console.log(selectli);
-// });
-
+            if (id === "1980년대" || id === "1990년대" || id === "2000년대" || id === "2010년대" || id === "2020년대" || id === "기타") {
+                year = value ? id : '';
+            } else if (id === "발라드" || id === "댄스" || id === "랩,힙합" || id === "R&B,Soul" || id === "인디음악" || id === "록,메탈" || id === "트로트" || id === "포크,블루스" || id === "기타" ) {
+                genre = value ? id : '';
+            } else if (id === "화창한날" || id === "비가내리는날" || id === "안개가가득한날" || id === "눈이내리는날" || id === "바람이부는날" || id === "기타" ) {
+                weather = value ? id : '';
+            } else if (id === "아침" || id === "점심" || id === "저녁" || id === "새벽" || id === "기타" ) {
+                time = value ? id : '';
+            }
+            const selectValue = [year, genre, weather, time];
+            question = selectValue.join();
+            console.log(question)
+    })
+})
 
