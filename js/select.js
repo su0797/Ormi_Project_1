6d1b60ded2d5e1018451fcfb4f566ed14c074b64
su12 <ts04109@naver.com>
2023-06-14 10:15:53 +0900

    js 수정

--- js/select.js
+++ js/select.js
@@ -12,16 +12,17 @@ document.querySelectorAll('input[type="radio"]').forEach((radio) => {
             const value = target.value;
 
             if (id === "1980년대" || id === "1990년대" || id === "2000년대" || id === "2010년대" || id === "2020년대" || id === "모든연도") {
-                year = value ? id : '';
+                year = id;
             } else if (id === "발라드" || id === "댄스" || id === "랩,힙합" || id === "R&B,Soul" || id === "인디음악" || id === "록,메탈" || id === "트로트" || id === "포크,블루스" || id === "모든장르" ) {
-                genre = value ? id : '';
+                genre = id;
             } else if (id === "화창한날" || id === "비가내리는날" || id === "안개가가득한날" || id === "눈이내리는날" || id === "바람이부는날" || id === "모든날씨" ) {
-                weather = value ? id : '';
+                weather = id;
             } else if (id === "아침" || id === "점심" || id === "저녁" || id === "새벽" || id === "모든시간대" ) {
-                time = value ? id : '';
+                time = id;
             }
             const selectValue = [year, genre, weather, time];
             question = selectValue.join();
+            console.log(question);
     })
 })
 
